c4b69159dd9fa57c9f62b7a84e9ef30acf02aa57
Qihao <qihaoliu001@gmail.com>
2024-07-23 15:35:25 -0700

    first push

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,20 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCebXii9Iv-eqsyavIongVE_SVnkqAa_Lk",
+    authDomain: "demoapp-e06fa.firebaseapp.com",
+    projectId: "demoapp-e06fa",
+    storageBucket: "demoapp-e06fa.appspot.com",
+    messagingSenderId: "460199505469",
+    appId: "1:460199505469:web:a05940d270ef565a7b0454",
+    measurementId: "G-K7JF317RC0"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const firestore = getFirestore();
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { auth, firestore };
